6a4c58d6917e0749c92c39c184126cbbeddce77f
Sangamesh B M <sangamesh71998@gmail.com>
2024-04-13 11:35:56 +0530

    Post Box -3

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,62 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show edit update destroy ]
+
+  # GET /messages or /messages.json
+  def index
+    @messages = Message.all
+  end
+
+  # GET /messages/1 or /messages/1.json
+  def show
+  end
+
+  # GET /messages/new
+  def new
+    @message = Message.new
+  end
+
+  # GET /messages/1/edit
+  def edit
+  end
+
+  # POST /messages or /messages.json
+  def create
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      redirect_to messages_path, notice: 'Message was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /messages/1 or /messages/1.json
+  def update
+    @message = current_user.messages.find(params[:id])
+    if @message.update(message_params)
+      redirect_to messages_path, notice: 'Message was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /messages/1 or /messages/1.json
+  def destroy
+    @message.destroy
+
+    respond_to do |format|
+      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
