3e64ee837a9e07cfe2f2e9cae82d3549d3d4f007
Sangamesh B M <sangamesh71998@gmail.com>
2024-04-13 13:05:49 +0530

    post box -6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_13_054937) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_13_071225) do
   create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
@@ -27,8 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_13_054937) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "messages", "users"
